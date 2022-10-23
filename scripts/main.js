5082dfe2589baafb813648f40230bd87840a9d67
Peter Phan <ipeterphan@gmail.com>
2022-10-23 02:24:27 -0400

    Changed to js

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+// let myModal = document.getElementById('myModal')
+// let myInput = document.getElementById('myInput')
+
+// myModal.addEventListener('shown.bs.modal', function() {
+//   myInput.focus()
+// })
