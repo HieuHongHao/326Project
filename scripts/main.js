1c4bbe278396353fd600cf65e9077e576259687f
Peter Phan <ipeterphan@gmail.com>
2022-11-03 02:10:49 -0400

    Dummy login

--- scripts/main.js
+++ scripts/main.js
@@ -24,6 +24,24 @@ toggle.onclick = function() {
   localStorage.setItem('theme', targetTheme);
 };
 
+function loginSuccess() {
 
+}
+
+async function login() {
+  const username = document.getElementById("loginName").value;
+  const password = document.getElementById("loginPass").value;
+  const res = await fetch("../users.json");
+  const users = await res.json();
+  const user = users.filter(x => x.name === username || x.email === username);
+  if (user.length !== 0 && user[0].password === password) {
+    loginSuccess();
+  } else {
+    alert("Username or password is incorrect")
+  }
+}
+
+const loginBtn = document.getElementById("loginSubmit");
+loginBtn.addEventListener("click", login);
 
 
