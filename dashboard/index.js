6a3a3b446a0b6714fe816a96995cdc9838d7224a
Peter Phan <ipeterphan@gmail.com>
2022-11-04 02:46:39 -0400

    Added pfp to dashboard

--- dashboard/index.js
+++ dashboard/index.js
@@ -5,12 +5,14 @@ async function loadDash() {
     const users = await res.json();
     const user = users.filter(x => x.id === parseInt(userId))[0];
 
+    const pfp = document.getElementById("pfp");
     const username = document.getElementById("username");
     const likes = document.getElementById("likes");
     const currPosts = document.getElementById("currPosts");
     const totalPosts = document.getElementById("totalPosts");
     const comments = document.getElementById("comments");
     const created = document.getElementById("created");
+    pfp.src = user.avatar;
     username.innerHTML = user.name;
     likes.innerHTML += user.likes + " Likes";
     currPosts.innerHTML += user.posts.length + " Current Projects";
