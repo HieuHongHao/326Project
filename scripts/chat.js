193d8929825dc15dd7c0393b7881437489a2444e
pete_win10dell <phatn@umass.edu>
2022-11-09 17:44:39 -0500

    fdnmsfno

--- scripts/chat.js
+++ scripts/chat.js
@@ -3,7 +3,9 @@ import { api } from './api.js';
 export const chat = {
     init: async () => {
         // const socket = io("http://localhost:9000");
-        const socket = io("https://cs326project.herokuapp.com:9000");
+        const PORT = process.env.PORT;
+        // const socket = io("https://cs326project.herokuapp.com:9000");
+        const socket = io("https://cs326project.herokuapp.com:" + PORT.toString());
         // const socket = io();
         // const socket = io("128.119.202.240:9000");
 
