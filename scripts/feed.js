b203d25ab71dda4ac6be8d9bbdca1777c284c2ec
Peter Phan <ipeterphan@gmail.com>
2022-11-09 21:42:40 -0500

    fixed likes counter

--- scripts/feed.js
+++ scripts/feed.js
@@ -78,8 +78,8 @@ export const feed = {
       const tags = createTag(data.tags);
       const content = document.createElement("div");
       const likeButton = document.createElement("button");
-      
-      
+
+
 
       title.classList.add("fs-4", "fw-bold", "m-0");
       title.innerHTML = data.title;
@@ -90,9 +90,9 @@ export const feed = {
       likeButton.className = "btn btn-outline-light mx-1 pb-3"
       likeButton.innerHTML = data.likes;
       likeButton.addEventListener("click", () => {
-        likeButton.innerHTML += 1;
+        likeButton.innerHTML = parseInt(likeButton.innerHTML) + 1;
       })
-      
+
 
       wrapper.classList.add("px-3");
       wrapper.appendChild(tags);
@@ -181,7 +181,7 @@ export const feed = {
       response_json.posts.forEach(post => postContainer.appendChild(createNewPost(post)));
     });
 
-    githubPostBtn.addEventListener("click", async () =>{
+    githubPostBtn.addEventListener("click", async () => {
       const response_json = await api.fetchData('github_repos');
       console.log(response_json);
       postContainer.replaceChildren();
