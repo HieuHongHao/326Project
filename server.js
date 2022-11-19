3f1a37b0f9f51c377374f10067791edcfb14dfbc
Peter Phan <ipeterphan@gmail.com>
2022-11-19 06:46:52 -0500

    Project and post api from db

--- server.js
+++ server.js
@@ -5,26 +5,20 @@ const morgan = require("morgan");
 const { Octokit } = require("octokit");
 const dotenv = require('dotenv');
 
-dotenv.config({path: "./.env"});
+dotenv.config({ path: "./.env" });
 const octokit = new Octokit();
 
 const Post = require("./Backend/model");
 
+const ProjectModel = require("./Backend/model/Post");
+const UserModel = require("./Backend/model/User");
+const CommentModel = require("./Backend/model/Comment");
 
-
-
-const {
-  UserService,
-  CommentService,
-  PostService,
-  CanvasService,
-} = require("./Backend/database");
-
-const moongoose = require('mongoose');
-const users = new UserService();
-const posts = new PostService();
-const comments = new CommentService();
-const canvases = new CanvasService();
+const mongoose = require('mongoose');
+// const users = new UserService();
+// const posts = new PostService();
+// const comments = new CommentService();
+// const canvases = new CanvasService();
 
 app.use(express.static(__dirname));
 
@@ -36,170 +30,254 @@ app.use(
   })
 );
 
-moongoose.connect(process.env.DATABASE_URL,function(err, connection){
-  if(err){
-      console.log(err.message);
+
+mongoose.connect(process.env.DATABASE_URL, function(err, connection) {
+  if (err) {
+    console.log(err.message);
   }
-  else{
-      console.log("Connection established");
+  else {
+    console.log("Connection established");
+  }
+})
+const database = mongoose.connection;
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected');
+})
+
+app.use(express.json());
+
+// Get all projects
+app.get("/api/projects", async (req, res) => {
+  try {
+    const data = await ProjectModel.find();
+    res.json(data)
+  }
+  catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+});
+
+// Get Project by ID
+app.get('/api/projects/:id', async (req, res) => {
+  try {
+    const data = await ProjectModel.findById(req.params.id);
+    res.json(data)
+  }
+  catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+// Get all Comments 
+app.get("/api/comments", async (req, res) => {
+  try {
+    const data = await CommentModel.find();
+    res.json(data)
+  }
+  catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+});
+
+// Get comments by author ID
+app.get('/api/comments/author/:id', async (req, res) => {
+  try {
+    const data = await CommentModel.find({ "author": req.params.id });
+    res.json(data)
+  }
+  catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+// Get comments by project ID
+app.get('/api/comments/project/:id', async (req, res) => {
+  try {
+    const data = await CommentModel.find({ "post": req.params.id });
+    res.json(data)
+  }
+  catch (error) {
+    res.status(500).json({ message: error.message })
   }
 })
 
 
 
 
-app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
 });
 
-app.get("/api/users", (req, res) => {
-  const filter = req.query ? req.query : {};
-  res.status(200).json({
-    status: "Success",
-    users: users.find(filter),
-  });
-});
+// app.get("/api/users", (req, res) => {
+//   const filter = req.query ? req.query : {};
+//   res.status(200).json({
+//     status: "Success",
+//     users: users.find(filter),
+//   });
+// });
 
-app.post("/api/users", (req,res) => {
-  const info = req.body;
-  users.insert(info);
-  res.status(200).json({
-    status: "Sucess",
-    user: info
-  })
-})
+// app.post("/api/users", (req, res) => {
+//   const info = req.body;
+//   users.insert(info);
+//   res.status(200).json({
+//     status: "Sucess",
+//     user: info
+//   })
+// })
 
-app.get("/api/users/:id", (req, res) => {
-  const userid = req.params.id;
-  
-  res.status(200).json({
-    status: "Success",
-    users: users.findById(userid)
-  });
-});
+// app.get("/api/users/:id", (req, res) => {
+//   const userid = req.params.id;
 
-app.delete("/api/users/:id", (req,res) => {
-  const userid = req.params.id;
-  users.delete(userid);
-  res.status(200).json({
-    status: `Deleted user ${userid}`,
-  });
-})
-app.get("/api/posts", (req, res) => {
-  const filter = req.query ? req.query : {};
-  res.status(200).json({
-    status: "Success",
-    posts: posts.find(filter),
-  });
-});
+//   res.status(200).json({
+//     status: "Success",
+//     users: users.findById(userid)
+//   });
+// });
 
-app.get("/api/canvas", (req, res) => {
-  const filter = req.query ? req.query : {};
-  res.status(200).json({
-    status: "Success",
-    posts: canvases.find(filter),
-  });
-});
+// app.delete("/api/users/:id", (req, res) => {
+//   const userid = req.params.id;
+//   users.delete(userid);
+//   res.status(200).json({
+//     status: `Deleted user ${userid}`,
+//   });
+// })
+// app.get("/api/posts", (req, res) => {
+//   const filter = req.query ? req.query : {};
+//   res.status(200).json({
+//     status: "Success",
+//     posts: posts.find(filter),
+//   });
+// });
 
-
-
-app.get("/api/canvas/:id", (req, res) => {
-  const canvasId = req.params.id;
-  res.status(200).json({
-    status: "Success",
-    comments: canvases.findById(canvasId),
-  });
-});
-
-app.get("/api/posts/:id/comments", (req, res) => {
-  const postId = req.params.id;
-  res.status(200).json({
-    status: "Sucess",
-    comments: posts.getAllComments(postId),
-  });
-});
+// app.get("/api/canvas", (req, res) => {
+//   const filter = req.query ? req.query : {};
+//   res.status(200).json({
+//     status: "Success",
+//     posts: canvases.find(filter),
+//   });
+// });
 
 
 
-app.get("/api/github_repos", async (req, res) => {
-  const response = await octokit.rest.search.repos({
-    q: "java in:topics",
-  });
-  const repos = response.data.items.slice(0, 6);
-  const posts = repos.map((repo) => {
-    return {
-      id: repo.id,
-      content: repo.description,
-      likes: repo.stargazers_count,
-      hearts: repo.watchers_count,
-      tags: repo.topics.map(
-        (tag) => tag[0].toUpperCase() + tag.slice(1, tag.length)
-      ),
-      title: repo.full_name,
-    };
-  });
-  console.log(repos);
-  res.status(200).json({
-    status: "Sucess",
-    posts
-  });
-});
+// app.get("/api/canvas/:id", (req, res) => {
+//   const canvasId = req.params.id;
+//   res.status(200).json({
+//     status: "Success",
+//     comments: canvases.findById(canvasId),
+//   });
+// });
 
-app.post("/api/posts/:id/comments", (req, res) => {
-  const postId = req.params.id;
-  const authorId = req.body.comment.authorId;
-  const content = req.body.comment.content;
-  const commentId = comments.counter;
-  const newComment = comments.insert({
-    authorId,
-    content,
-    postId,
-    id: commentId,
-  });
-  comments.counter += 1;
-  res.status(200).json({
-    status: "Success",
-    post: posts.addComment(postId, commentId),
-  });
-});
-
-app.get("/api/posts/:id", (req, res) => {
-  const postId = req.params.id;
-  res.status(200).json({
-    status: "Success",
-    comments: posts.findById(postId),
-  });
-});
-
-app.post("/api/posts", (req, res) => {
-  const newPost = req.body.newPost;
-  newPost["id"] = posts.counter;
-  posts.counter += 1;
-  posts.insert(newPost);
-  res.status(200).json({
-    status: "Success",
-    post: newPost,
-  });
-});
-
-app.put("/api/posts/:id", (req, res) => {
-  res.status(200).json({
-    status: "Success",
-    post: posts.findByIdAndUpdate(req.params.id, req.body.update),
-  });
-});
+// app.get("/api/posts/:id/comments", (req, res) => {
+//   const postId = req.params.id;
+//   res.status(200).json({
+//     status: "Sucess",
+//     comments: posts.getAllComments(postId),
+//   });
+// });
 
 
 
-app.delete("/api/posts/:id", (req, res) => {
-  posts.delete(req.params.id)
-  res.status(200).json({
-    status: `Deleted post ${req.params.id}`,
-  });
-})
+// app.get("/api/github_repos", async (req, res) => {
+//   const response = await octokit.rest.search.repos({
+//     q: "java in:topics",
+//   });
+//   const repos = response.data.items.slice(0, 6);
+//   const posts = repos.map((repo) => {
+//     return {
+//       id: repo.id,
+//       content: repo.description,
+//       likes: repo.stargazers_count,
+//       hearts: repo.watchers_count,
+//       tags: repo.topics.map(
+//         (tag) => tag[0].toUpperCase() + tag.slice(1, tag.length)
+//       ),
+//       title: repo.full_name,
+//     };
+//   });
+//   console.log(repos);
+//   res.status(200).json({
+//     status: "Sucess",
+//     posts
+//   });
+// });
+
+// app.post("/api/posts/:id/comments", (req, res) => {
+//   const postId = req.params.id;
+//   const authorId = req.body.comment.authorId;
+//   const content = req.body.comment.content;
+//   const commentId = comments.counter;
+//   const newComment = comments.insert({
+//     authorId,
+//     content,
+//     postId,
+//     id: commentId,
+//   });
+//   comments.counter += 1;
+//   res.status(200).json({
+//     status: "Success",
+//     post: posts.addComment(postId, commentId),
+//   });
+// });
+
+// app.get("/api/posts/:id", (req, res) => {
+//   const postId = req.params.id;
+//   res.status(200).json({
+//     status: "Success",
+//     comments: posts.findById(postId),
+//   });
+// });
+
+// app.post("/api/posts", (req, res) => {
+//   const newPost = req.body.newPost;
+//   newPost["id"] = posts.counter;
+//   posts.counter += 1;
+//   posts.insert(newPost);
+//   res.status(200).json({
+//     status: "Success",
+//     post: newPost,
+//   });
+// });
+
+// app.put("/api/posts/:id", (req, res) => {
+//   res.status(200).json({
+//     status: "Success",
+//     post: posts.findByIdAndUpdate(req.params.id, req.body.update),
+//   });
+// });
+
+
+
+// app.delete("/api/posts/:id", (req, res) => {
+//   posts.delete(req.params.id)
+//   res.status(200).json({
+//     status: `Deleted post ${req.params.id}`,
+//   });
+// })
 
 
 
@@ -279,7 +357,7 @@ io.on("connection", (socket) => {
 
 
 httpServer.listen(process.env.PORT || 9000, () =>
-  console.log("Server running on port" + process.env.PORT)
+  console.log("Server running on port " + (process.env.PORT || 9000))
 );
 
 
