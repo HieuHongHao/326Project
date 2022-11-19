0b259a47ff4cd87f15171632684b9950dea26e93
Hieu Nguyen <hietnguyen@umass.edu>
2022-11-19 13:16:31 -0500

    Fix search by tags

--- scripts/feed.js
+++ scripts/feed.js
@@ -183,7 +183,7 @@ export const feed = {
       let result;
       switch (query[0]) {
         case "tag":
-          result = await api.fetchData(URL + `/projects?tag=${query[1]}`);
+          result = await api.fetchData(URL + `/projects?tags=${query[1]}`);
           break;
         case "title":
           result = await api.fetchData(URL + `/projects?title=${query[1]}`);
