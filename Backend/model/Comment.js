df61189a2adeb41afcc5a58df2ae852a81914734
Peter Phan <ipeterphan@gmail.com>
2022-11-19 06:50:30 -0500

    Changed post -> project

--- Backend/model/Comment.js
+++ Backend/model/Comment.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  post: {
+  project: {
     type: Schema.Types.ObjectId,
     ref: "Project"
   },
